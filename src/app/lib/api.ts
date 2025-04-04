c237c1d31d4c274e3bf4769391ed9ad75615f438
Rafael Cassaro Costa <rafaelcassacosta@hotmail.com>
2025-04-04 08:53:35 -0300

    fixing api bug

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -12,7 +12,7 @@ export const getRepos = async (username: string) : Promise<reposApi[] | null> =>
     const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
 
     if (!response.ok) {
-        throw new Error("Erro ao buscar os repositórios");
+        return null;
     }
 
     const repos: reposApi[] = await response.json();
